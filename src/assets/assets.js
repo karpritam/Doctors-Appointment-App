cb26d7c797e043e1c5b4e4bc2d388b4a969ee1b8
karpritam <karpritam194@gmail.com>
2026-04-10 00:40:32 +0530

    initial commit

---
+++ src/assets/assets.js
@@ -0,0 +1,328 @@
+import doc1 from "./doc1.png";
+import doc2 from "./doc2.png";
+import doc3 from "./doc3.png";
+import doc4 from "./doc4.png";
+import doc5 from "./doc5.png";
+import doc6 from "./doc6.png";
+import doc7 from "./doc7.png";
+import doc8 from "./doc8.png";
+import doc9 from "./doc9.png";
+import doc10 from "./doc10.png";
+import doc11 from "./doc11.png";
+import doc12 from "./doc12.png";
+import doc13 from "./doc13.png";
+import doc14 from "./doc14.png";
+import doc15 from "./doc15.png";
+import about_image from "./about_image.png";
+import appointment_img from "./appointment_img.png";
+import contact_image from "./contact_image.png";
+import cross_icon from "./cross_icon.png";
+import profile_pic from "./profile_pic.png";
+import razorpay_logo from "./razorpay_logo.png";
+import stripe_logo from "./stripe_logo.png";
+import upload_area from "./upload_area.png";
+import upload_icon from "./upload_icon.png";
+import header_img from "./header_img.png";
+import group_profiles from "./group_profiles.png";
+import add_icon from "./add_icon.svg";
+import admin_logo from "./admin_logo.svg";
+import appointment_icon from "./appointment_icon.svg";
+import appointments_icon from "./appointments_icon.svg";
+import cancel_icon from "./cancel_icon.svg";
+import doctor_icon from "./doctor_icon.svg";
+import earning_icon from "./earning_icon.svg";
+import home_icon from "./home_icon.svg";
+import list_icon from "./list_icon.svg";
+import patient_icon from "./patient_icon.svg";
+import patients_icon from "./patients_icon.svg";
+import people_icon from "./people_icon.svg";
+import tick_icon from "./tick_icon.svg";
+import upload_area2 from "./upload_area.svg";
+import arrow_icon from "./arrow_icon.svg";
+import chats_icon from "./chats_icon.svg";
+import Darmatologist from "./Darmatologist.svg";
+import Gastroenterologist from "./Gastroenterologist.svg";
+import General_physician from "./General_physician.svg";
+import Gynecologist from "./Gynecologist.svg";
+import Neurologist from "./Neurologist.svg";
+import Pediatricians from "./Pediatricians.svg";
+import info_icon from "./info_icon.svg";
+import logo from "./logo.svg";
+import menu_icon from "./menu_icon.svg";
+import verified_icon from "./verified_icon.svg";
+import dropdown_icon from "./dropdown_icon.svg";
+
+export const assets = {
+	header_img,
+	appointment_img,
+	logo,
+	group_profiles,
+	chats_icon,
+	verified_icon,
+	info_icon,
+	profile_pic,
+	arrow_icon,
+	contact_image,
+	about_image,
+	menu_icon,
+	cross_icon,
+	dropdown_icon,
+	upload_icon,
+	stripe_logo,
+	razorpay_logo,
+};
+
+export const specialityData = [
+	{
+		speciality: "General Physician",
+		image: General_physician,
+	},
+	{
+		speciality: "Gynocologist",
+		image: Gynecologist,
+	},
+	{
+		speciality: "Dermatologist",
+		image: Darmatologist,
+	},
+	{
+		speciality: "Pediatricians",
+		image: Pediatricians,
+	},
+	{
+		speciality: "Neurologist",
+		image: Neurologist,
+	},
+	{
+		speciality: "Gastroenterologist",
+		image: Gastroenterologist,
+	},
+];
+
+export const doctors = [
+	{
+		_id: "doc1",
+		name: "Dr. Richanrd James",
+		image: doc1,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc2",
+		name: "Dr. Richanrd James",
+		image: doc2,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc3",
+		name: "Dr. Richanrd James",
+		image: doc3,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc4",
+		name: "Dr. Richanrd James",
+		image: doc4,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc5",
+		name: "Dr. Richanrd James",
+		image: doc5,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc6",
+		name: "Dr. Richanrd James",
+		image: doc6,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc7",
+		name: "Dr. Richanrd James",
+		image: doc7,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc8",
+		name: "Dr. Richanrd James",
+		image: doc8,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc9",
+		name: "Dr. Richanrd James",
+		image: doc9,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc10",
+		name: "Dr. Richanrd James",
+		image: doc10,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc11",
+		name: "Dr. Richanrd James",
+		image: doc11,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc12",
+		name: "Dr. Richanrd James",
+		image: doc12,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc13",
+		name: "Dr. Richanrd James",
+		image: doc13,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc14",
+		name: "Dr. Richanrd James",
+		image: doc14,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+	{
+		_id: "doc15",
+		name: "Dr. Richanrd James",
+		image: doc15,
+		speciality: "Geberal Physician",
+		degree: "MBBS",
+		experience: "4 Years",
+		about:
+			"Dr. Davis a strong commitment to delivering comprehensive medical care",
+		fees: 500,
+		address: {
+			line1: "17th cross, Richmond",
+			line2: "Circle,Ring Road, London",
+		},
+	},
+];
