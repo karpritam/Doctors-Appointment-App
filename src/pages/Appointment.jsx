ec7c305588ed695c85aef70f306b2efe9a0dd4af
karpritam <karpritam194@gmail.com>
2026-04-14 01:00:03 +0530

    docotrs page done

--- src/pages/Appointment.jsx
+++ src/pages/Appointment.jsx
@@ -1,11 +1,71 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { AppContext } from "../context/AppContext";
+import { assets } from "../assets/assets";
 
 const Appointment = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const { docId } = useParams();
+	const [docInfo, setDocInfo] = useState(null);
+	const { doctors, currencySymbol } = useContext(AppContext);
+	const [docSlots, setDocSlots] = useState([]);
+	const [slotsIndex, setSlotIndex] = useState(0);
+	const [slotsTime, setSlotsTime] = useState("");
 
-export default Appointment
+	const fetchDocInfo = async () => {
+		const docInfo = doctors.find((doc) => doc._id === docId);
+		setDocInfo(docInfo);
+		console.log(docInfo);
+	};
+	useEffect(() => {
+		fetchDocInfo();
+	}, [doctors, docId]);
+	return (
+		docInfo && (
+			<div>
+				{/* -----Doctor Details---- */}
+				<div className="flex flex-col gap-4 sm:flex-row">
+					<div>
+						<img
+							className="bg-primary w-full sm:max-w-72 rounded-lg"
+							src={docInfo.image}
+							alt=""
+						/>
+					</div>
+					<div className="flex-1 border border-gray-400 rounded-lg p-8 py-7 bg-white mx-2 sm:mx-0 mt-[-80px] sm:mt-0">
+						{/* .----doc info--- */}
+						<p className="flex items-center gap-2 text-2xl font-medium text-gray-900">
+							{docInfo.name}{" "}
+							<img className="w-5" src={assets.verified_icon} alt="" />
+						</p>
+						<div className="flex items-center gap-2 text-sm mt-1 text-gray-600">
+							<p>
+								{docInfo.degree} - {docInfo.speciality}
+							</p>
+							<button className="py-0.5 px-2 border text-xs rounded-full">
+								{docInfo.experience}
+							</button>
+						</div>
+						{/* doctor about */}
+						<div className="">
+							<p className="flex items-center gap-1 text-sm font-medium text-gray-900 mt-3">
+								About <img src={assets.info_icon} alt="" />
+							</p>
+							<p className="text-sm text-gray-500 mt-1 max-w-[700px]">
+								{docInfo.about}
+							</p>
+						</div>
+						<p className="text-gray-500 font-medium mt-4">
+							Appointment fee:{" "}
+							<span className="text-gray-600">
+								{currencySymbol}
+								{docInfo.fees}
+							</span>
+						</p>
+					</div>
+				</div>
+			</div>
+		)
+	);
+};
+
+export default Appointment;
